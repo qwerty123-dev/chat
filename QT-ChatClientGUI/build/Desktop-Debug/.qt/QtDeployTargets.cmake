05a05bc6a9b0e96e34284adb2a0ef17e83ff9411
qwerty <qwerty@qwerty.internet>
2025-07-06 16:52:14 +1200

    Delete useless build

--- QT-ChatClientGUI/build/Desktop-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_QT-ChatClientGUI_FILE /home/qwerty/QT-ChatClientGUI/build/Desktop-Debug/QT-ChatClientGUI)
-set(__QT_DEPLOY_TARGET_QT-ChatClientGUI_TYPE EXECUTABLE)
--- QT-ChatClientGUI/build/Desktop-Debug/.qt/deploy_QT_ChatClientGUI_a58ede9d43.cmake
+++
@@ -1,8 +0,0 @@
-include("/home/qwerty/QT-ChatClientGUI/build/Desktop-Debug/.qt/QtDeploySupport.cmake")
-include("${CMAKE_CURRENT_LIST_DIR}/QT-ChatClientGUI-plugins.cmake" OPTIONAL)
-set(__QT_DEPLOY_I18N_CATALOGS "qtbase")
-
-qt6_deploy_runtime_dependencies(
-    EXECUTABLE /home/qwerty/QT-ChatClientGUI/build/Desktop-Debug/QT-ChatClientGUI
-    GENERATE_QT_CONF
-)
